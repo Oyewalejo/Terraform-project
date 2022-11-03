29d05d66f748689d19cae334b874a49b45d51e0b
Oyewale <oyewalejoyetunji@gmail.com>
2022-11-03 00:46:20 -0400

    created vpc.tf and variable.tf files

---
+++ variable.tf
@@ -0,0 +1,47 @@
+variable "vpc_cidr_block" {
+  default = "10.0.0.0/16"
+  description = "VPC cidr block"
+  type = string
+}
+
+variable "public_subnet_az1_cidr_block" {
+  default = "10.0.0.0/24"
+  description = "public subnet AZ1 cidr block"
+  type = string
+}
+
+variable "public_subnet_az2_cidr_block" {
+  default = "10.0.1.0/24"
+  description = "public subnet AZ2 cidr block"
+  type = string
+}
+
+variable "public_route_table_cidr_block" {
+  default = "0.0.0.0/0"
+  description = "public route table cidr block"
+  type = string
+}
+
+variable "private-app-subnet-az1_cidr_block" {
+  default = "10.0.2.0/24"
+  description = "private app subnet AZ1 cidr block"
+  type = string
+}
+
+variable "private-app-subnet-az2_cidr_block" {
+  default = "10.0.3.0/24"
+  description = "private app subnet AZ2 cidr block"
+  type = string
+}
+
+variable "private-data-subnet-az1_cidr_block" {
+  default = "10.0.4.0/24"
+  description = "private data subnet AZ1 cidr block"
+  type = string
+}
+
+variable "private-data-subnet-az2_cidr_block" {
+  default = "10.0.5.0/24"
+  description = "private data subnet AZ2 cidr block"
+  type = string
+}
